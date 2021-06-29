dd256f7e2b16950cd737bd1cc0e18ecc58d9b9e9
unknown <kierastevens0@gmail.com>
2021-06-29 17:57:02 +0100

    Kiera adding consent form

--- App/iframe_library.js
+++ App/iframe_library.js
@@ -19,17 +19,9 @@
 */
 
 var libraries = '<meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate" />' +
-                '<scr' + 'ipt src="https://code.jquery.com/jquery-3.2.1.min.js"></scr' + 'ipt>' +
-								'<scr' + 'ipt src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></scr' + 'ipt>'+
-
-								'<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">' +
-								'<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.css"></link>'+
-								'<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.theme.min.css"></link>'+
-								'<scr' + 'ipt src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></scr' + 'ipt>'+
-								'<scr' + 'ipt src="https://cdnjs.cloudflare.com/ajax/libs/jstat/1.7.1/jstat.min.js"></scr' + 'ipt>'+
-
-								'<scr' + 'ipt src="https://code.jquery.com/ui/1.12.1/jquery-ui.min.js" integrity="sha256-VazP97ZCwtekAsvgPBSUwPFKdrwD3unUfSGVYrahUqU="   crossorigin="anonymous"></scr' + 'ipt>'+
-								'<scr' + 'ipt src = "https://cdnjs.cloudflare.com/ajax/libs/bootbox.js/4.4.0/bootbox.min.js"></scr' + 'ipt>'+
-								'<scr' + 'ipt src = "https://cdnjs.cloudflare.com/ajax/libs/PapaParse/4.6.1/papaparse.min.js"></scr' + 'ipt>'+
-								'<scr' + 'ipt src = "https://cdnjs.cloudflare.com/ajax/libs/echarts/4.0.4/echarts.min.js"></scr' + 'ipt>'+
-								'<scr' + 'ipt src = "https://cdnjs.cloudflare.com/ajax/libs/Chart.js/1.0.2/Chart.min.js"></scr' + 'ipt>';
+    '<scr' + 'ipt src="libraries/jquery-3.6.0.min.js"></scr' + 'ipt>' +
+		'<scr' + 'ipt src="libraries/popper.min.js"></scr' + 'ipt>'+
+		'<link rel="stylesheet" href="libraries/bootstrapCollector.css">' +
+		'<scr' + 'ipt src="libraries/bootstrap.5.0.1.min.js"></scr' + 'ipt>'+
+    '<scr' + 'ipt src="libraries/bootbox.5.4.min.js"></scr' + 'ipt>'+
+		'<scr' + 'ipt src="libraries/papaparse.4.3.6.min.js"></scr' + 'ipt>';
